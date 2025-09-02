c0b45fefb8e5fb43a9c8931758045515bafd904a
Luna Ortiz <loortiz.3617@unicesmag.edu.co>
2025-09-02 15:38:17 -0500

    table users

---
+++ Data base/table_users.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users(
+	id BIGSERIAL PRIMARY KEY,
+	firstname varchar(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	mobile_numbre VARCHAR(20) NOT NULL,
+	ide_number VARCHAR(15) NULL UNIQUE,
+	adress TEXT NULL,
+	birthday DATE NULL,
+	email VARCHAR(200) NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	status BOOLEAN NOT NULL DEFAULT TRUE,
+	created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+	deleted_at TIMESTAMPTZ NULL
+);
+
+INSERT INTO users(
+	firstname,
+	lastname,
+	mobile_numbre,
+	email,
+	password
+ )
+VALUES(
+	'Luna',
+	'Ortiz',
+	'37632184',
+	'dhsj@d',
+	'13232'
+);
